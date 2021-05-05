c797c3a42b594315912a5bf6f04d23d87338711b
alexander matiugin <alexander.matiugin@pandadoc.com>
2021-05-05 13:43:24 +0300

    add validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,26 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   readonly firstName: string;
+
+  @IsString()
   readonly lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
   readonly password: string;
 }
